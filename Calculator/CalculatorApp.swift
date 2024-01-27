da7040a5c2c526444cf479091432f73c10e150a9
kraiglarson2 <kraigalarson2@gmail.com>
2024-01-27 11:08:26 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Krajg Larson on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
